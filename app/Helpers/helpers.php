99773f72b47a5d7378a16e15c89fa1e5be88c2ab
Jango <teamdark657@gmail.com>
2026-04-24 20:23:16 +0100

    add icon.txt file

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -38,3 +38,80 @@ if (!function_exists('sec_asset')) {
         return secure_asset($path);
     }
 }
+
+if (!function_exists('icon_classes')) {
+    function icon_classes(): array
+    {
+        static $icons = null;
+
+        if ($icons !== null) {
+            return $icons;
+        }
+
+        $iconsPath = storage_path('app/icons.txt');
+        $icons = [];
+
+        if (is_readable($iconsPath)) {
+            $lines = file($iconsPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) ?: [];
+            $icons = array_map(static function ($line) {
+                return trim($line, " \t\n\r\0\x0B\",");
+            }, $lines);
+        }
+
+        if (empty($icons)) {
+            $icons = font_awesome_solid_icon_classes();
+        }
+
+        if (empty($icons)) {
+            $icons = [
+                'fa-solid fa-bullseye',
+                'fa-solid fa-layer-group',
+                'fa-solid fa-briefcase',
+                'fa-solid fa-chart-line',
+                'fa-solid fa-location-dot',
+                'fa-solid fa-star',
+                'fa-solid fa-handshake',
+                'fa-solid fa-globe',
+                'fa-solid fa-lightbulb',
+                'fa-solid fa-users',
+            ];
+        }
+
+        $icons = array_filter($icons);
+        $icons = array_unique($icons);
+
+        return $icons = array_values($icons);
+    }
+}
+
+if (!function_exists('font_awesome_solid_icon_classes')) {
+    function font_awesome_solid_icon_classes(): array
+    {
+        $iconsDirectory = public_path('lib/font-awesome-6.5.0/svgs/solid');
+        $iconFiles = glob($iconsDirectory . '/*.svg') ?: [];
+
+        $icons = array_map(static function ($iconFile) {
+            return 'fa-solid fa-' . pathinfo($iconFile, PATHINFO_FILENAME);
+        }, $iconFiles);
+
+        sort($icons);
+
+        return $icons;
+    }
+}
+
+if (!function_exists('random_icon_classes')) {
+    function random_icon_classes(int $limit = 45, ?string $currentIcon = null): array
+    {
+        $icons = icon_classes();
+        shuffle($icons);
+        $icons = array_slice($icons, 0, $limit);
+
+        $currentIcon = trim((string) $currentIcon);
+        if ($currentIcon !== '' && !in_array($currentIcon, $icons, true)) {
+            array_unshift($icons, $currentIcon);
+        }
+
+        return $icons;
+    }
+}
